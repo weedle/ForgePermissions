fceca95618ce6c614e63644b62d11a19b8b9743b
Raekye <withoutpointk@gmail.com>
2013-04-26 07:43:15 -0700

    reorganizing JSONConfigurationTransformer

--- src/minecraft/com/creatifcubed/minecraft/forgepermissions/mod/models/JSONConfigurationTransformer.java
+++ src/minecraft/com/creatifcubed/minecraft/forgepermissions/mod/models/JSONConfigurationTransformer.java
@@ -6,16 +6,17 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.creatifcubed.minecraft.forgepermissions.ForgePermissions;
-
 import net.minecraftforge.common.ConfigCategory;
 import argo.format.JsonFormatter;
 import argo.format.PrettyJsonFormatter;
 import argo.jdom.JdomParser;
+import argo.jdom.JsonArrayNodeBuilder;
 import argo.jdom.JsonNodeBuilders;
 import argo.jdom.JsonObjectNodeBuilder;
 import argo.jdom.JsonRootNode;
 import argo.saj.InvalidSyntaxException;
+
+import com.creatifcubed.minecraft.forgepermissions.ForgePermissions;
 
 public class JSONConfigurationTransformer implements ConfigurationTransformer {
 
@@ -29,7 +30,7 @@ public class JSONConfigurationTransformer implements ConfigurationTransformer {
 
 		JsonObjectNodeBuilder catsNode = JsonNodeBuilders.anObjectBuilder();
 		builder.withField("categories", catsNode);
-		
+
 		JsonRootNode node = builder.build();
 		System.out.println(formatter.format(node));
 	}
@@ -39,24 +40,30 @@ public class JSONConfigurationTransformer implements ConfigurationTransformer {
 		JsonRootNode root = new JdomParser().parse(new FileReader(f));
 
 	}
-	
+
 	private class JsonTree {
 		private Map<String, Object> nodes;
+
 		public JsonTree() {
 			this.nodes = new HashMap<String, Object>();
 		}
-		
+
 		public void put(String key, Object value) {
 			if (key.isEmpty()) {
-				throw new IllegalArgumentException("JSONConfigurationTransformer.JsonTree.put(String, Object) string param 0 is empty");
+				throw new IllegalArgumentException(
+						"JSONConfigurationTransformer.JsonTree.put(String, Object) string param 0 is empty");
+			}
+			Class valueClass = value.getClass().getComponentType() == null ? value.getClass() : value.getClass().getComponentType();
+			if (!Number.class.isAssignableFrom(valueClass) && !String.class.isAssignableFrom(valueClass) && !Number.class.isAssignableFrom(valueClass)) {
+				throw new IllegalArgumentException(String.format("JSONConfigurationTransformer.JsonTree.put(String, Object) string param 0 value class invalid type {%s}", value.getClass().getName()));
 			}
 			String[] parts = key.split("\\.");
 			Map<String, Object> container = this.ensurePathExists(parts);
 			container.put(parts[parts.length - 1], value);
 		}
-		
-		public Map<String, Object> ensurePathExists(String[] parts) {
-			Map<String, Object> last = nodes;
+
+		private Map<String, Object> ensurePathExists(String[] parts) {
+			Map<String, Object> last = this.nodes;
 			for (int i = 0; i < parts.length - 1; i++) {
 				Object current = ((Map) last).get(parts[i]);
 				if (!(current instanceof Map)) {
@@ -67,63 +74,58 @@ public class JSONConfigurationTransformer implements ConfigurationTransformer {
 			}
 			return last;
 		}
-		
+
 		public JsonRootNode build() {
-			JsonObjectNodeBuilder builder = JsonNodeBuilders.anObjectBuilder();
-			for (String node : nodes.keySet()){
-				Object nodeVal = nodes.get(node);
+			JsonObjectNodeBuilder root = JsonNodeBuilders.anObjectBuilder();
+			for (String node : this.nodes.keySet()) {
+				Object nodeVal = this.nodes.get(node);
 				
-				if (nodeVal instanceof Integer){
-					builder = buildInteger(builder, node, (Integer) nodeVal);
-					
-				} else if (nodeVal instanceof String){
-					builder = buildString(builder, node, (String) nodeVal);
-					
-				} else if (nodeVal instanceof Boolean){
-					builder = buildBoolean(builder, node, (Boolean) nodeVal);
-					
-				} else if (nodeVal instanceof Integer[]){
-					for (int i : (Integer[]) nodeVal){
-						builder = buildInteger(builder, node, i);
+				if (nodeVal instanceof Integer) {
+					root = this.buildNumber(root, node, (Integer) nodeVal);
+				} else if (nodeVal instanceof String) {
+					root = this.buildString(root, node, (String) nodeVal);
+				} else if (nodeVal instanceof Boolean) {
+					root = this.buildBoolean(root, node, (Boolean) nodeVal);
+				} else if (nodeVal instanceof Number[] || nodeVal instanceof String[] || nodeVal instanceof Boolean[]) {
+					JsonArrayNodeBuilder array = JsonNodeBuilders.anArrayBuilder();
+					if (nodeVal instanceof Integer[]) {
+						for (int i : (Integer[]) nodeVal) {
+							root = this.buildNumber(root, node, i);
+						}
+					} else if (nodeVal instanceof String[]) {
+						for (String s : (String[]) nodeVal) {
+							root = this.buildString(root, node, s);
+						}
+					} else if (nodeVal instanceof Boolean[]) {
+						for (Boolean b : (Boolean[]) nodeVal) {
+							root = this.buildBoolean(root, node, b);
+						}
 					}
-					
-				} else if (nodeVal instanceof String[]){
-					for (String s : (String[]) nodeVal){
-						builder = buildString(builder, node, s);
-					}
-					
-				} else if (nodeVal instanceof Boolean[]){
-					for (Boolean b : (Boolean[]) nodeVal){
-						builder = buildBoolean(builder, node, b);
-					}				
-					
-				} else {
+					root.withField(node, array);
+				} else if (nodeVal == null) {
 					ForgePermissions.log.info(String.format("Got null key {%s}", node));
-					builder = builder.withField(node, JsonNodeBuilders.aNullBuilder());
-				} 
+					root = root.withField(node, JsonNodeBuilders.aNullBuilder());
+				}
 			}
-			return builder.build();
+			return root.build();
 		}
-		
-		private JsonObjectNodeBuilder buildBoolean(
-		JsonObjectNodeBuilder builder, String key, Boolean value) {
-			if(value){
+
+		private JsonObjectNodeBuilder buildBoolean(JsonObjectNodeBuilder builder, String key, Boolean value) {
+			if (value) {
 				builder = builder.withField(key, JsonNodeBuilders.aTrueBuilder());
 			} else {
 				builder = builder.withField(key, JsonNodeBuilders.aFalseBuilder());
 			}
-				return builder;
+			return builder;
 		}
 
-		private JsonObjectNodeBuilder buildString(
-		JsonObjectNodeBuilder builder, String key, String value) {
+		private JsonObjectNodeBuilder buildString(JsonObjectNodeBuilder builder, String key, String value) {
 			builder = builder.withField(key, JsonNodeBuilders.aStringBuilder(value));
 			return builder;
 		}
 
-		public JsonObjectNodeBuilder buildInteger(
-		JsonObjectNodeBuilder builder, String key, Integer value){
-			builder = builder.withField(key, JsonNodeBuilders.aNumberBuilder("\"" + value.toString() + "\""));
+		private JsonObjectNodeBuilder buildNumber(JsonObjectNodeBuilder builder, String key, Number value) {
+			builder = builder.withField(key, JsonNodeBuilders.aNumberBuilder(value.toString()));
 			return builder;
 		}
 	}
